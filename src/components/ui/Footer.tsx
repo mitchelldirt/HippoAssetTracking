6c65e69cd3c05d8c91bf541fb924147a5f813159
Mitchell Mudd <developer@mitchellmudd.dev>
2023-12-12 19:18:30 -0500

    Fixed underline spacing in the footer and fixed hamburger menu not being tabbable with keyboard in the header

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -6,33 +6,33 @@ export default function Footer () {
             <div className={'flex flex-col items-center justify-center'}>
                 <ul className={'w-full h-full flex flex-col items-center justify-between'}>
                     <li className={'w-4/5 sm:w-3/5'}>
-                        <a className={'text-left'} href={''}>About the author</a>
+                        <a className={'text-left hover:underline decoration-black decoration-wavy decoration-2 underline-offset-4'} href={''}>About the author</a>
                     </li>
 
                     <li className={'w-4/5 sm:w-3/5'}>
-                        <a className={'text-left'}>Privacy Policy</a>
+                        <a className={'text-left hover:underline decoration-black decoration-wavy decoration-2 underline-offset-4'} href={''}>Privacy Policy</a>
                     </li>
                 </ul>
             </div>
             <div className={'flex flex-col items-center justify-center'}>
                 <ul className={'w-full h-full flex flex-col items-center justify-between'}>
                     <li className={'w-4/5 sm:w-3/5'}>
-                        <a className={'text-left'}>Terms of Service</a>
+                        <a className={'text-left hover:underline decoration-black decoration-wavy decoration-2 underline-offset-4'} href={''}>Terms of Service</a>
                     </li>
 
                     <li className={'w-4/5 sm:w-3/5'}>
-                        <a className={'text-left'}>FAQ</a>
+                        <a className={'text-left hover:underline decoration-black decoration-wavy decoration-2 underline-offset-4'} href={''}>FAQ</a>
                     </li>
                 </ul>
             </div>
             <div className={'flex pt-8 sm:pt-0 flex-row col-span-2 sm:col-span-1 w-full items-center justify-around'}>
-                <a>
+                <a href="https://github.com/mitchelldirt/HippoAssetTracking" target="_blank">
                     <GithubIcon className={'w-8 h-8'} />
                 </a>
-                <a>
+                <a href="https://www.linkedin.com/in/mitchellmudd/" target="_blank">
                     <LinkedinIcon className={'w-8 h-8'} />
                 </a>
-                <a>
+                <a href="https://www.mitchellmudd.dev/" target="_blank">
                     <GlobeIcon className={'w-8 h-8'} />
                 </a>
             </div>
